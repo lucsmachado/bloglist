6d3637f5db51afe7d7fac6750cfed5404b6af1be
Lucas Machado <73367620+lucsmachado@users.noreply.github.com>
2022-03-18 18:00:24 -0300

    Fixed order of imports

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+const config = require('./utils/config');
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
-const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
 
 mongoose.connect(config.MONGODB_URI);
